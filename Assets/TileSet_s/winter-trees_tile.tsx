371db209d42562a6ea8cd3744ee77c7007429c8f
Abdenourtheprogrammer95 <sator.abdenour66@outlook.com>
2025-12-12 16:27:43 +0100

    Adding assets.

---
+++ Assets/TileSet_s/winter-trees_tile.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="winter-trees_tile" tilewidth="528" tileheight="576" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../Assets/dying_tree.png" width="528" height="576"/>
+ </tile>
+ <tile id="1">
+  <image source="../Assets/pine.png" width="528" height="528"/>
+ </tile>
+</tileset>
