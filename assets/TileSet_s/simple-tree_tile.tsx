f4d5136b950db21531a5e1c20bc60ed762f17528
Abdenourtheprogrammer95 <sator.abdenour66@outlook.com>
2025-12-12 17:29:23 +0100

    Reorganizing.

---
+++ assets/TileSet_s/simple-tree_tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="simple-tree_tile" tilewidth="168" tileheight="280" tilecount="4" columns="2">
+ <image source="../Images/simple_tree.png" width="336" height="560"/>
+</tileset>
